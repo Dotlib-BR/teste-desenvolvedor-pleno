68edb0b3cb12faaced5a31647c9364c5017ff8c1
Vlamir Santo <vlamirsanto@gmail.com>
2019-09-23 16:40:42 -0300

    instalando dependencias e start no projeto
    Atualização da descrição do teste.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost"
+	});
+	
+	server.route({
+		method: 'GET',
+		path: '/products',
+		handler: (request, h) => {
+
+				return 'Hello World!';
+		}
+	});
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
