758c1453841e12a67ef9dad6f292ac6f542a6779
Rafael <rafael.mouraweb@gmail.com>
2019-10-03 01:32:54 -0300

    criando rota put

--- src/handlers/todos.js
+++ src/handlers/todos.js
@@ -15,6 +15,10 @@ const getAll = async (request, h) => {
   const todos = await TodoModel.find({})
   return { data: todos.map(transformer) }
 }
+const find = async (request, h) => {
+  const todo = await TodoModel.findById(request.params.id)
+  return {data: transformer(todo)}
+}
 
 const save  = async (request, h) => {
   const { name } = request.payload
@@ -25,7 +29,19 @@ const save  = async (request, h) => {
   return h.response(transformer(todo)).code(201)
 }
 
+const remove  = async (request, h) => {
+  await TodoModel.findByIdAndDelete({ _id: request.params.id})
+  return h.response().code(204)
+}
+const update  = async (request, h) => {
+  await TodoModel.findByIdAndUpdate({ _id: request.params.id}, {name: request.payload.name})
+  return h.response().code(204)
+}
+
 module.exports = {
   getAll,
-  save
+  find,
+  save,
+  remove,
+  update
 }
