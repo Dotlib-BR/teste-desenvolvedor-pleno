95d7ab23b5e49c4d457c0ca7c702fb3f80ab88ef
Rafael <rafael.mouraweb@gmail.com>
2019-10-03 01:32:54 -0300

    criando rota put

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,23 @@ module.exports = [
     handler: todosHandler.getAll
   },
   {
+    method: "GET",
+    path: "/api/v1/todos/{id}",
+    handler: todosHandler.find
+  },
+  {
     method: "POST",
     path: "/api/v1/todos",
     handler: todosHandler.save
-  }
+  },
+  {
+    method: "DELETE",
+    path: "/api/v1/todos/{id}",
+    handler: todosHandler.remove
+  },
+  {
+    method: "PUT",
+    path: "/api/v1/todos/{id}",
+    handler: todosHandler.update
+  }    
 ];
