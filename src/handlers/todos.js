d18ebcef6980500c091ec987dd781d1c35fd9d32
Rafael <rafael.mouraweb@gmail.com>
2019-10-02 21:46:01 -0300

    Criando o handler get

--- src/handlers/todos.js
+++ src/handlers/todos.js
@@ -1,28 +1,28 @@
 const TodoModel = require('../models/todo')
 
-const getAll  = (request, h) => {
-  return 'Olá Programador!!!'
+const transformer =  todoResponse => ({
+  type: 'todos',
+  id: todoResponse.id,
+  attributes: {
+    name: todoResponse.name
+  },
+  links: {
+    self: `/api/v1/todos/${todoResponse.id}`
+  }
+})
+
+const getAll = async (request, h) => {
+  const todos = await TodoModel.find({})
+  return { data: todos.map(transformer) }
 }
 
 const save  = async (request, h) => {
   const { name } = request.payload
-
   const todo = new TodoModel
   todo.name = name
-  
   await todo.save()
 
-  todoResponse = {
-    type: 'todos',
-    id: todo.id,
-    attributes: {
-      name: todo.name
-    },
-    links: {
-      self: `/api/v1/todos/${todo.id}`
-    }
-  }
-  return h.response(todoResponse).code(201)
+  return h.response(transformer(todo)).code(201)
 }
 
 module.exports = {
