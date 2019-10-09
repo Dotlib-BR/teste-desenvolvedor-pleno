94963847746184b642356e785edf5e1126c19327
Pedro Paradella <pedro.paradella@inpartec.com.br>
2019-10-09 15:22:43 -0300

    Implementado metodo put para rota /todos/:todoID

--- routers/todoRouter.js
+++ routers/todoRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 function routes(Todo) {
     const todoRouter = express.Router();
-
+    //Rota para todos os todo's (Com metodos get e post)
     todoRouter.route('/todos')
         .post((req, res) => {
             if (req.body.task && req.body.person && req.body.date) {
@@ -25,7 +25,7 @@ function routes(Todo) {
                 return res.json(todos);
             });
         });
-
+    //Middleware para interceptar a requisição e tratar antes de ir para a rota /todos/:todoID
     todoRouter.use('/todos/:todoID', (req, res, next) => {
         Todo.findById(req.params.todoID, (err, todo) => {
             if (err) {
@@ -38,10 +38,23 @@ function routes(Todo) {
             return res.sendStatus(404);
         });
     });
-
+    //Rota um todo's (Com metodos get, put, patch e delete)
     todoRouter.route('/todos/:todoID')
         .get((req, res) => {
             return res.json(req.todo);
+        })
+        .put((req, res) => {
+            const { todo } = req;
+            todo.task = req.body.task;
+            todo.person = req.body.person;
+            todo.date = req.body.date;
+            todo.done = req.body.done;
+            todo.save((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.json(todo);
+            });
         });
 
     return todoRouter;
