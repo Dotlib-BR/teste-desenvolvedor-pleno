3267accf5f956343323a2f7cd6ea3823c5fb5d20
Pedro Paradella <pedro.paradella@inpartec.com.br>
2019-10-09 15:02:26 -0300

    Implementado metodo post

--- routers/todoRouter.js
+++ routers/todoRouter.js
@@ -4,14 +4,23 @@ function routes(Todo) {
     const todoRouter = express.Router();
 
     todoRouter.route('/todos')
+        .post((req, res) => {
+            if (req.body.task && req.body.person && req.body.date) {
+                const todo = new Todo(req.body);
+
+                todo.save();
+                return res.status(201).json(todo);
+            }
+            return res.sendStatus(400);
+        })
         .get((req, res) => {
             const query = {};
             if (req.query.done) {
                 query.done = req.query.done;
             }
             Todo.find(query, (err, todos) => {
-                if (err){
-                  return res.send(err);
+                if (err) {
+                    return res.send(err);
                 }
                 return res.json(todos);
             });
